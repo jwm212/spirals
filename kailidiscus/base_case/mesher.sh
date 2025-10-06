fbb2b36cd6648cd419881b3991b1ea043063fc88
James McDermott <james.mcdermott5@nhm.ac.uk>
2025-10-06 08:43:37 +0100

    kailidiscus meshing script

---
+++ kailidiscus/base_case/mesher.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+##### RUNNING THIS WILL DELETE ANY PREVIOUS DATA IN THESE CASES #####
+
+#SBATCH --partition=short
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=6G
+
+apptainer run --bind ~/projects/nhm/jmcdermo/:/mnt --pwd /mnt/spirals/kailidiscus/base_case ~/apps/openfoam_12.sif bash -c "
+foamCleanTutorials &&
+blockMesh &&
+snappyHexMesh -overwrite | tee log.snappyHexMesh &&
+checkMesh | tee log.checkMesh" #&&
+#foamToVTK -faceSet wrongFaces -time 0"
