d26bcffcf8f8747773e418e2535ce91c803776a0
James McDermott <james.mcdermott5@nhm.ac.uk>
2025-10-14 17:16:10 +0100

    gogia spiralis base case initial setup

---
+++ gogia_spiralis/mesher.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+##### RUNNING THIS WILL DELETE ANY PREVIOUS DATA IN THESE CASES #####
+
+#SBATCH --partition=short
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+
+apptainer run --bind ~/projects/nhm/jmcdermo/:/mnt --pwd /mnt/spirals/gogia_spiralis/base_case ~/apps/openfoam_12.sif bash -c "
+foamCleanTutorials &&
+blockMesh &&
+snappyHexMesh -overwrite | tee log.snappyHexMesh &&
+checkMesh | tee log.checkMesh"
