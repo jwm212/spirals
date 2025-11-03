3f88a886ef02ec1981a5aa23fe0b2ebe1e0e9c36
James McDermott <james.mcdermott5@nhm.ac.uk>
2025-11-03 16:33:53 +0000

    set up and run gogia palmeri velocity study

---
+++ gogia_palmeri/velocity/scripts/solver.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#SBATCH --array=0-1
+#SBATCH --partition=long
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=8G
+
+
+#cases=(v0.05 v0.1 v0.2 v0.3 v0.4 v0.5)
+cases=(v0.4 v0.5)
+apptainer run --bind ~/projects/nhm/jmcdermo/:/mnt --pwd /mnt/spirals/gogia_palmeri/velocity/${cases[$SLURM_ARRAY_TASK_ID]} ~/apps/openfoam_12.sif bash -c "
+decomposePar &&
+mpirun --use-hwthread-cpus -n 24 --oversubscribe icoFoam -parallel | tee log.icoFoam &&
+reconstructPar &&
+postProcess -func vorticity &&
+foamLog log.icoFoam &&
+foamToVTK"
+
+# Remove processor dirs:
+rm -rf ../${cases[$SLURM_ARRAY_TASK_ID]}/processor*
+
+# plot residuals and save inside each case file in postProcessing/
+echo "plotting residuals..."
+cd ../${cases[$SLURM_ARRAY_TASK_ID]}
+gnuplot ../scripts/residuals.gnu
+
+
+#compress VTK files ready for transfer
+cd $TMPDIR
+echo "copying .vtk files..."
+time cp ~/projects/nhm/jmcdermo/spirals/gogia_palmeri/velocity/${cases[$SLURM_ARRAY_TASK_ID]}/VTK/*.vtk .
+echo "compressing VTK files..."
+time tar -cvf VTKs.tar *.vtk
+time pigz -p 24 VTKs.tar
+
+mv VTKs.tar.gz ~/projects/nhm/jmcdermo/spirals/gogia_palmeri/velocity/${cases[$SLURM_ARRAY_TASK_ID]}/VTK
+echo "done"
